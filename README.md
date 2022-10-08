79435f9ab72937ea2775264bbb373a6cf0ef2bab
Huỳnh Quang Vinh <vinhhq@esuhai.com>
2022-10-08 09:20:02 +0700

    update link

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-#Link tham khảo
-[x] https://viblo.asia/p/nodejs-tao-qr-code-trong-nodejs-voi-qrcode-L4x5x61mZBM
-[x] https://www.npmjs.com/package/qrcode
-[x] https://www.npmjs.com/package/qrcode-generator
-[x] https://blog.minhazav.dev/HTML5-QR-Code-scanning-support-for-local-file-and-default-camera/
-[x] https://www.npmjs.com/package/html5-qrcode
-[x] https://blog.minhazav.dev/research/html5-qrcode#scan-using-file
-[x] https://dev.to/sbodi10/download-images-using-javascript-51a9
+##QRCode generator and scanner on browser.
--- src/App.js
+++ src/App.js
@@ -95,3 +95,12 @@ function App() {
 }
 
 export default App
+
+//Link tham khảo
+//[x] https://viblo.asia/p/nodejs-tao-qr-code-trong-nodejs-voi-qrcode-L4x5x61mZBM
+//[x] https://www.npmjs.com/package/qrcode
+//[x] https://www.npmjs.com/package/qrcode-generator
+//[x] https://blog.minhazav.dev/HTML5-QR-Code-scanning-support-for-local-file-and-default-camera/
+//[x] https://www.npmjs.com/package/html5-qrcode
+//[x] https://blog.minhazav.dev/research/html5-qrcode#scan-using-file
+//[x] https://dev.to/sbodi10/download-images-using-javascript-51a9
