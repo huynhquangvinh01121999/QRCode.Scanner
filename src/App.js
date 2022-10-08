e40ce0c4f065a50184e13caf528f0dd94b7e97b6
Huỳnh Quang Vinh <vinhhq@esuhai.com>
2022-10-08 09:18:08 +0700

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import QRCode from 'qrcode'
+import { Html5Qrcode } from 'html5-qrcode'
 
 function App() {
+  const [urlQrCode, setUrlQrCode] = useState("")
+  const [value, setValue] = useState("")
+
+  const generateQrCode = async () => {
+    try {
+      var url = await QRCode.toDataURL(value)
+      setUrlQrCode(url)
+      console.log(url);
+    } catch (err) { }
+  }
+
+  const generateQrCodeKeydown = async (e) => {
+    if (e.keyCode == 13) {
+      try {
+        var url = await QRCode.toDataURL(value)
+        setUrlQrCode(url)
+        console.log(url);
+      } catch (err) { }
+    }
+  }
+
+  const generateQrCodeFromJson = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await response.json();
+    var url = await QRCode.toDataURL(JSON.stringify(data[0]))
+    setUrlQrCode(url)
+    setValue(JSON.stringify(data[0]))
+    console.log(url);
+  }
+
+  const handleScanQrCode = (e) => {
+    const html5QrCode = new Html5Qrcode("reader");
+    if (e.target.files.length == 0) {
+      return;
+    }
+
+    const imageFile = e.target.files[0];
+    html5QrCode.scanFile(imageFile, true)
+      .then(data => {
+        setValue(data);
+        setUrlQrCode("")
+      })
+      .catch(err => {
+        console.log(`Error scanning file. Reason: ${err}`)
+      });
+    html5QrCode.clear();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h3 className='title'>Generate QR Code</h3>
+      <div className="main">
+        <textarea rows={8} value={value}
+          placeholder="Input text to convert QR"
+          onChange={e => setValue(e.target.value)}
+          onKeyDown={generateQrCodeKeydown}></textarea>
+
+        <button
+          onClick={generateQrCode}
+          style={{ marginLeft: "5px" }}>Generator QR Code
+        </button>
+
+        <button
+          onClick={generateQrCodeFromJson}
+          style={{ marginTop: "10px" }}>Generator QR Code From JSON API
+        </button>
+
+        <button
+          onClick={() => window.location.reload()}
+          style={{ marginLeft: "150px", marginTop: "10px" }}>Reload page
+        </button>
+
+        <div style={{ marginTop: "20px" }}>
+          <img src={urlQrCode} alt="null" /><br />
+          <a
+            href={urlQrCode}
+            download={value ? true : false}>Download QR Code
+          </a>
+        </div>
+      </div>
+      <hr />
+
+      {/* Read QR Code from Image file */}
+      <div id="reader"></div>
+      <input type="file"
+        accept="image/*"
+        onChange={handleScanQrCode} />
+    </>
+  )
 }
 
-export default App;
+export default App
